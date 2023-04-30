a26ad21db03ec45ebd23466c4d462477ce2b330d
Eva Frankovic <emf017@bucknell.edu>
2023-04-30 17:26:03 -0400

    Adding new screen and buttons

---
+++ src/main/java/org/team04/blackjackmvc/BlackjackGameOverContoller.java
@@ -0,0 +1,54 @@
+package org.team04.blackjackmvc;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class BlackjackGameOverContoller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Label lblEndBalance;
+
+    @FXML
+    void initialize() {
+        assert homeButton != null : "fx:id=\"homeButton\" was not injected: check your FXML file 'blackJackGameOver.fxml'.";
+        assert lblEndBalance != null : "fx:id=\"lblEndBalance\" was not injected: check your FXML file 'blackJackGameOver.fxml'.";
+    }
+
+
+    /**
+     * {@link ActionEvent} handler for when we're ready to change to the next
+     * scene.
+     *
+     * @param event is the {@link ActionEvent} passed
+     */
+    @FXML
+    void onBtnNextSceneAction(ActionEvent event) {
+        // Get the Stage object of this button
+        Stage stage = (Stage) homeButton.getScene().getWindow();
+
+        if (event.getSource() == homeButton)
+            blackjackMain.loadSceneOnStage(stage, FXMLScenes.PLAY);
+
+    }
+
+    @FXML
+    void setBalance(){
+
+    }
+}
+
